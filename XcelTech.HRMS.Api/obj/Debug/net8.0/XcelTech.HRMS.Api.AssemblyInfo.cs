cb3abac66459cb7b048014423053072040ba0ddc
amanuel <amanuelbeyne662@gmail.com>
2024-05-11 23:31:49 +0300

    fixed docker persistence issue

--- XcelTech.HRMS.Api/obj/Debug/net8.0/XcelTech.HRMS.Api.AssemblyInfo.cs
+++ XcelTech.HRMS.Api/obj/Debug/net8.0/XcelTech.HRMS.Api.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("XcelTech.HRMS.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3790d0c081829905c8e3a8a94f75d4c456bb632")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d92b388227493c7618ab6937200b1382c3bbcc78")]
 [assembly: System.Reflection.AssemblyProductAttribute("XcelTech.HRMS.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("XcelTech.HRMS.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- XcelTech.HRMS.Bloc/obj/Debug/net8.0/XcelTech.HRMS.Bloc.AssemblyInfo.cs
+++ XcelTech.HRMS.Bloc/obj/Debug/net8.0/XcelTech.HRMS.Bloc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("XcelTech.HRMS.Bloc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3790d0c081829905c8e3a8a94f75d4c456bb632")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d92b388227493c7618ab6937200b1382c3bbcc78")]
 [assembly: System.Reflection.AssemblyProductAttribute("XcelTech.HRMS.Bloc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("XcelTech.HRMS.Bloc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
