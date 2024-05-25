d7ca22d4c5d5dec3117bdfd35142416bb8610360
amanuel <amanuelbeyne662@gmail.com>
2024-05-25 18:08:20 +0300

    asfdsadf

---
+++ XcelTech.HRMS.Repo/ApplicationDbContext.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using XcelTech.HRMS.Model;
+using XcelTech.HRMS.Model.Model;
+using Microsoft.AspNetCore.Identity;
+
+namespace XcelTech.HRMS.Repo
+{
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                //optionsBuilder.UseNpgsql("Server=localhost;Database=Hrms;User Id=postgres;Password=Portal_0;");
+
+                
+                optionsBuilder.UseNpgsql("Host=xcelTech.hrms.repo;Port=5432;Database=xceltechhrms;Username=postgres;Password=postgres;Include Error Detail=true;");
+
+            }
+        }
+
+        public DbSet<Attendance> Attendances { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Leave> Leaves { get; set; }
+        public DbSet<Payroll> Payrolls { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Recruitment> Recruitments { get; set; }
+        public DbSet<Training> Training { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            List<IdentityRole> roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Name = "admin",
+                    NormalizedName = "ADMIN"
+                },
+                new IdentityRole
+                {
+                    Name = "buyer",
+                    NormalizedName = "BUYER"
+                },
+                new IdentityRole
+                {
+                    Name = "seller",
+                    NormalizedName = "SELLER"
+                }
+            };
+
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
+    }
+}
---
+++ XcelTech.HRMS.Repo/Migrations/20240428111946_migrate to dockker postgres.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace XcelTech.HRMS.Repo.Migrations
+{
+    /// <inheritdoc />
+    public partial class migratetodockkerpostgres : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "6272e728-ef50-4b31-8264-c36dde6c998b");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "655c46ac-1139-468c-924d-5c2afecf21dc");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "8d4d3ae0-2a58-42d8-8656-61d19a0f97b9");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "36a73835-d02f-4206-8a33-fd81eb9e2ea5", null, "buyer", "BUYER" },
+                    { "37b1ce70-cc66-42e0-9a56-e7033cbfc46b", null, "seller", "SELLER" },
+                    { "b18dced5-03dc-4ec3-b971-766da791b069", null, "admin", "ADMIN" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "36a73835-d02f-4206-8a33-fd81eb9e2ea5");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "37b1ce70-cc66-42e0-9a56-e7033cbfc46b");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "b18dced5-03dc-4ec3-b971-766da791b069");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "6272e728-ef50-4b31-8264-c36dde6c998b", null, "admin", "ADMIN" },
+                    { "655c46ac-1139-468c-924d-5c2afecf21dc", null, "buyer", "BUYER" },
+                    { "8d4d3ae0-2a58-42d8-8656-61d19a0f97b9", null, "seller", "SELLER" }
+                });
+        }
+    }
+}
